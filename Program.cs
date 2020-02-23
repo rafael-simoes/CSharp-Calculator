fbdd20efdc3c25bb256e498634f309c69d852c8e
rafael-simoes <blazerafa@gmail.com>
2020-02-23 11:25:37 +0000

    Basic C# Calculator
    Basic calculator made on C# Console

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Operation (+ - * /) = ");
+            string new_operator = Console.ReadLine();
+            Console.Write("y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            switch (new_operator)
+             {
+                 case "+":
+                     Console.WriteLine("Result = {0}", x + y);
+                     break;
+                case "-":
+                    Console.WriteLine("Result = {0}", x - y);
+                    break;
+                case "*":
+                    Console.WriteLine("Result = {0}", x * y);
+                    break;
+                case "/":
+                    Console.WriteLine("Result = {0}", x / y);
+                    break;
+             }
+            
+        }
+    }
+}
